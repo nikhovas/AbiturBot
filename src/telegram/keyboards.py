f9a6cfa6483800280560d373386c7e00bd6ebb5e
t1mursadykov <t1mursadykov@mail.ru>
2020-02-29 17:05:15 +0300

    getting phone number

--- src/telegram/keyboards.py
+++ src/telegram/keyboards.py
@@ -1,5 +1,9 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 markup_request = ReplyKeyboardMarkup(resize_keyboard=True).add(
     KeyboardButton('Отправить свой контакт ☎️', request_contact=True)
 )
+
+
+async def get_users_departments():
+    pass
--- src/telegram/telegram_messenger.py
+++ src/telegram/telegram_messenger.py
@@ -1,10 +1,11 @@
-from src.messenger import Messenger
 from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import ReplyKeyboardRemove
+from aiogram.utils import executor
+
+from src.messenger import Messenger
 from . import keyboards
-# from src.app_config import kernel
 
 bot = Bot(token='909308261:AAHJmfqOW2D5-epx5XePYHRuVuEgVML4Odw')
 dispatcher = Dispatcher(bot, storage=MemoryStorage())
@@ -21,5 +22,11 @@ class TelegramMessenger(Messenger):
     @dispatcher.message_handler(commands=['start'])
     async def process_start_command(msg: types.Message):
         await bot.send_message(msg.from_user.id, "Добро пожаловать! Отправьте свой номер телефона.",
-                                    reply_markup=keyboards.markup_request)
+                               reply_markup=keyboards.markup_request)
 
+    @dispatcher.message_handler(content_types=['contact'])
+    async def process_get_contact(msg: types.Message):
+        await bot.send_message(msg.from_user.id, "номер телефона {}".format(msg.contact['phone_number']),
+                               reply_markup=ReplyKeyboardRemove())
+
+        # data base working
