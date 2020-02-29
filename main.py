8f48650d74aea2e1609f1ee407c600cb5e17c96e
t1mursadykov <t1mursadykov@mail.ru>
2020-02-29 14:27:39 +0300

    nothing works

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.telegram.telegram_messenger import TelegramMessenger
-
+from src import kernel
 import asyncio
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    tg = TelegramMessenger(loop)
+    kernel = kernel.Kernel()
+    tg = TelegramMessenger(loop, kernel)
     tg.start_pooling()
--- src/telegram/telegram_messenger.py
+++ src/telegram/telegram_messenger.py
@@ -2,29 +2,29 @@ from aiogram import Bot, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-#from app_config import BOT_CONFIG
 
-import asyncio
 
 import src.telegram.keyboards as keyboards
 from src.messenger import Messenger
 
 
 class TelegramMessenger(Messenger):
+    bot = Bot(token='909308261:AAHJmfqOW2D5-epx5XePYHRuVuEgVML4Odw')
+    dispatcher = None
+
     def __init__(self, event_loop, kernel: Kernel):
         # super().__init__(kernel)
         super().__init__(kernel)
         self._event_loop = event_loop
 
-        self._bot = Bot(token='909308261:AAHJmfqOW2D5-epx5XePYHRuVuEgVML4Odw')
-        self._dispatcher = Dispatcher(self._bot, loop=event_loop, storage=MemoryStorage())
+        self.dispatcher = Dispatcher(self.bot, loop=event_loop, storage=MemoryStorage())
 
     def start_pooling(self):
-        executor.start_polling(self._dispatcher)
+        executor.start_polling(self.dispatcher)
 
-    @self._dispatcher.message_handler(commands=['start'])
+    @dispatcher.message_handler(commands=['start'])
     async def process_start_command(self, msg: types.Message):
-        await self._bot.send_message("Добро пожаловать! Отправьте свой номер телефона.",
+        await self.bot.send_message("Добро пожаловать! Отправьте свой номер телефона.",
                                      reply_markup=keyboards.markup_request)
 
 
