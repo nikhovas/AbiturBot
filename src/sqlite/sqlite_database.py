b2e8ffe604875adc7733b944ea022e4f87411302
t1mursadykov <t1mursadykov@mail.ru>
2020-03-01 00:23:26 +0300

    commit admins api

--- src/sqlite/sqlite_database.py
+++ src/sqlite/sqlite_database.py
@@ -1,21 +1,24 @@
-from src.database import Database
-from sqlalchemy import create_engine
-import aiopg
-from src.models import *
 from typing import List
+
+import aiopg
+
+from src.database import Database
+from src.models import *
 
 
 class SqliteDatabase(Database):
     def __init__(self, kernel):
         super().__init__()
 
-        self._dbname = "some name"
-        self._user = "user"
+        self._dbname = "db1"
+        self._user = "user1"
         self._password = "11111111"
-        self._host = "fds"
-
-        self._conn = None
+        self._host = "rc1c-5z1z7shu9girllca.mdb.yandexcloud.net:6432"
+        self._port = "6432"
+        self._dsn = 'dbname=db1 user=user1 password=11111111 host=rc1c-5z1z7shu9girllca.mdb.yandexcloud.net port=6432'
+        self._pool = None
         self._cur = None
+        self._conn = None
 
     async def connect(self):
         self._conn = await aiopg.connect(database=self._dbname,
@@ -28,11 +31,11 @@ class SqliteDatabase(Database):
         return self._conn is not None
 
     async def get_user_by_phone(self, phone: str) -> int:
-        if not self.is_connected():
-            await self.connect()
-
-        await self._cur.execute('SELECT user_id FROM bot_user where phone = {}'.format(phone))
-        return await self._cur.fetchone()[0]
+        async with aiopg.create_pool(self._dsn) as pool:
+            async with self._pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute('SELECT user_id FROM bot_user where phone = \'{}\''.format(phone))
+                    return (await self._cur.fetchone())[0]
 
     async def get_user_by_chat_id(self, chat_id: int):
         if not self.is_connected():
@@ -100,15 +103,17 @@ class SqliteDatabase(Database):
         return result
 
     async def set_chat_id_for_user(self, user_id: int, chat_id: int):
-        if not self.is_connected():
-            await self.connect()
+        async with aiopg.create_pool(self._dsn) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cur:
 
-        await self._cur.execute('SELECT chat_id FROM user_id_x_chat_id where user_id = {}'.format(user_id))
+                    await cur.execute('SELECT chat_id FROM user_id_x_chat_id where user_id = {}'.format(user_id))
 
-        database_chat_id = self._cur.fetchone()
+                    database_chat_id = await cur.fetchone()
 
-        if database_chat_id is None:
-            await self._cur.execute('INSERT INTO user_id_x_chat_id(user_id, chat_id) VALUES ({}, {})',
-                                    (user_id, chat_id))
-        elif database_chat_id is not chat_id:
-            await self._cur.execute('UPDATE user_id_x_chat_id SET chat_id = {} where user_id = {}'.format(chat_id, user_id))
+                    if database_chat_id is None:
+                        await cur.execute('INSERT INTO user_id_x_chat_id(user_id, chat_id) VALUES (%s, %s)',
+                                                (user_id, chat_id))
+                    elif database_chat_id is not chat_id:
+                        await cur.execute(
+                            'UPDATE user_id_x_chat_id SET chat_id = {} where user_id = {}'.format(chat_id, user_id))
