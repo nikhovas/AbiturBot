cc06951a04390983d9634cfe88980f5db573d7ea
nikhovas <nikhovas@ya.ru>
2020-02-29 12:59:29 +0300

    architecture

--- src/database.py
+++ src/database.py
@@ -1,5 +1,14 @@
 from src.module import Module
+from src.models import *
+from typing import List
 
 
 class Database(Module):
-    pass
+    async def get_phys_tech_schools_info(self):
+        pass
+
+    async def get_full_competition_list(self) -> List[Direction]:
+        pass
+
+    async def get_place_in_competition(self, user: User, competition: Direction) -> CompetitionInfo:
+        pass
