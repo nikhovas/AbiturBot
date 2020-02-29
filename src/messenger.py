6c747d2a5bcce6f6b3e65757d333235eb48fa154
nikhovas <nikhovas@ya.ru>
2020-02-29 13:22:13 +0300

    architecture

--- src/messenger.py
+++ src/messenger.py
@@ -4,10 +4,6 @@ from src.module import Module
 
 class Messenger(Module):
     # allows exceptions
-    async def send_raw_message(self, user: User, message: str):
-        pass
-
-    # allows exceptions
     # !!! use temp files (system directory /tmp) for temporary photos storing
     async def send_message_with_photos(self, user: User, message: str, attachments_files: list):
         pass
