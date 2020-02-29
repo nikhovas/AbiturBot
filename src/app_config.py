61610152815ca83739ff92ded2e550303d471941
t1mursadykov <t1mursadykov@mail.ru>
2020-02-29 12:57:31 +0300

    first bot comm

--- src/app_config.py
+++ src/app_config.py
@@ -1,7 +1,10 @@
 import src
 
-
 USING_MODULES = {
     'database': src.sqlite.sqlite_database.SqliteDatabase,
     'messenger': src.telegram.telegram_messenger.TelegramMessenger
+}
+
+BOT_CONFIG = {
+    'API_TOKEN' : '909308261:AAHJmfqOW2D5-epx5XePYHRuVuEgVML4Odw'
 }
--- src/telegram/telegram_messenger.py
+++ src/telegram/telegram_messenger.py
@@ -1,5 +1,19 @@
+from aiogram import Bot, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import Dispatcher
+from app_config import BOT_CONFIG
+
 from src.messenger import Messenger
 
 
 class TelegramMessenger(Messenger):
-    pass
+    def __init__(self, event_loop, kernel: Kernel):
+        super().__init__(kernel)
+        self._event_loop = event_loop
+
+        self._bot = BOT(token=BOT_CONFIG['API_TOKEN'])
+        self._dispatcher = Dispatcher(bot, storage=MemoryStorage())
+
+    @self._dispatcher.message_handler(commands=['start'])
+    async def process_start_command(self, msg: types.Message):
+        pass
